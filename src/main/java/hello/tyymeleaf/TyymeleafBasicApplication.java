1c35108e86ffbedf90df69db252c451333776f9e
SEO-my <muyear.seo@gmail.com>
2022-05-24 17:51:29 +0900

    master

---
+++ src/main/java/hello/tyymeleaf/TyymeleafBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.tyymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyymeleafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyymeleafBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/tyymeleaf/TyymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.tyymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyymeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
