8c1e964fafd99e9bab3c91499cdc2ea848973051
SEO-my <muyear.seo@gmail.com>
2022-05-29 19:12:01 +0900

    基本表現式

--- src/main/java/hello/tyymeleaf/basic/BasicController.java
+++ src/main/java/hello/tyymeleaf/basic/BasicController.java
@@ -1,10 +1,14 @@
 package hello.tyymeleaf.basic;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,6 +51,45 @@ public class BasicController {
 		
 		return "basic/variable";
 		
+	}
+	
+	@GetMapping("/basic-objects")
+	public String basicObjects(HttpSession session) {
+		session.setAttribute("sessionData", "Hello Session");
+		return "basic/basic-objects";
+	}
+	
+	@Component("helloBean")
+	static class HelloBean {
+		public String hello(String data) {
+			return "Hello " + data;
+		}
+	}
+	
+	@GetMapping("/date")
+	public String date(Model model) {
+		model.addAttribute("localDateTime", LocalDateTime.now());
+		return "basic/date";
+	}
+	
+	@GetMapping("link")
+	public String link(Model model) {
+		model.addAttribute("param1", "data1");
+		model.addAttribute("param2", "data2");
+		return "basic/link";
+	}
+	
+	@GetMapping("/literal")
+	public String literal(Model model) {
+		model.addAttribute("data", "Spring!");
+		return "basic/literal";
+	}
+	
+	@GetMapping("/operation")
+	public String operation(Model model) {
+		model.addAttribute("nullData", null);
+		model.addAttribute("data", "spring!");
+		return "basic/operation";
 	}
 	
 	@Data
